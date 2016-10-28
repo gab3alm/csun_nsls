51620d4406acd90f1517395176060ad67bee0bd8
Gabriel Almendarez <gab.alm94@gmail.com>
2016-10-27 22:43:54 -0700

    SNT land changed to sidebar layout, I think it looks so much better

---
+++ client/imports/snt_land/snt-group-card.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+import {MaterializeDirective} from 'angular2-materialize';
+
+import template from './snt-group-card.html';
+@Component({
+  selector:'snt-group-card',
+  template,
+  styleUrls:['styles/snt-group-card.css'],
+  directives:[MaterializeDirective]
+})
+
+export class SNTgroupCardComponent{}
--- client/imports/snt_land/snt-land.component.ts
+++ client/imports/snt_land/snt-land.component.ts
@@ -1,10 +1,26 @@
 import {Component} from '@angular/core';
+import {SNTgroupCardComponent} from './snt-group-card.component';
+import {MaterializeDirective} from 'angular2-materialize';
+
 
 import template from './snt-land.html';
 @Component({
   selector:'snt-land',
   template,
-  styleUrls:['styles/snt-land.css']
+  styleUrls:['styles/snt-land.css'],
+  directives:[SNTgroupCardComponent, MaterializeDirective]
 })
 
-export class SNTlandComponent{}
+export class SNTlandComponent{
+  // fields for SNT group creation form  
+  group_name:string;
+  first_name:string;
+  last_name:string;
+  email:string;
+  availability:string[];
+  
+  constructor(){
+
+  }
+
+}
