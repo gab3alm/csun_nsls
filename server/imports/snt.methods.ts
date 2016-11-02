2f6da6fe5622b77c4bbc4174ff70fa975f71ee0a
Gabriel Almendarez <gab.alm94@gmail.com>
2016-11-02 09:34:26 -0700

    Email notification for add/message have been implemented

--- server/imports/snt.methods.ts
+++ server/imports/snt.methods.ts
@@ -15,13 +15,16 @@ Meteor.methods({
       'auth_code': auth_code,
       'members':[founder]
     });
+    sendAuthCodeNotification(founder);
   },
 
   //add member to SNT group
   addToSntGroup: function(id:string, member:snt_member){
     SNTgroups.update({'_id':id}, {$addToSet : 
       { 'members' : member} 
-    });  
+    });
+    sendAuthCodeNotification(member);
+    newSntMemberNotification(id, member, "A new member has joined your Success Networking Team!");  
   },
 
   // send groups message to group's members
@@ -36,10 +39,38 @@ Meteor.methods({
             to: members[j].email,
             from: members[i].firstname + " " + members[i].lastname,
             subject: 'SNT Group Message',
-            text: message
+            text: message + " SENT BY - " + members[i].firstname + " " + members[i].lastname,
           });
         }
       }
     }
   }
 });
+
+
+// send email to entire team when new member joins their SNT group.
+function newSntMemberNotification(group_id:string, member:snt_member, email_subject:string){
+  // get members of the group
+  var members = SNTgroups.find({'_id':group_id}).fetch()[0].members;
+  for(var i =0; i < members.length; i++){
+    // send email to member that is not the current addition to the group
+    if(members[i].auth_Code != member.auth_Code){
+      Email.send({
+        to:members[i].email,
+        from:'csun-nsls',
+        subject:email_subject,
+        text: "Name: " + member.firstname + " " + member.lastname + " has joined your Success Networking Team, please contact your new member at their email address " + member.email
+      });    
+    }
+  }
+}
+
+// send email containing Auth Code to new member
+function sendAuthCodeNotification(member:snt_member){
+  Email.send({
+    to:member.email,
+    from:'csun-nsls',
+    subject:"Success Networking Team Auth Code",
+    text: "Hello " + member.firstname + ", We are so happy to see that you have joined/created a Success Networking Team. Here is your Auth Code, " + member.auth_Code + ". With this code, you will be able to send emails to your entire SNT group within the csun-nsls website, please keep in mind that this code will also allow you to remove your information from this SNT group if at some point you decide to leave. We hope you have a wonderful time in your SNT group."
+  });
+}
--- server/main.ts
+++ server/main.ts
@@ -9,3 +9,4 @@ import {ServerSession} from 'meteor/matteodem:server-session';
 Meteor.onConnection(function(connection){
   ServerSession.set('clientIP',connection.clientAddress);
 });
+
